b008b81adb25c7c70eed5961b0a13a6273882a2a
Chelsea Hsiao <48115431+chelseahsiao@users.noreply.github.com>
2019-05-19 11:38:17 -0700

    updated the app component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent
-implements OnInit{
-  current: IcurrentWeather
- constructor() {
-    this.current = {
-      city: 'Bethesda',
-      country: 'US',
-      date: new Date(),
-      image:'',
-      temperture:'72'
-      description:'Sunny'
-    }
-
-    
-
-
-
+export class AppComponent {
+  title = 'local-weather-app';
+}
